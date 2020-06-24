224a46d153dd920b2e617caf872239c4a46a757d
Bryce Whittington <brycew30@gmail.com>
2020-06-24 17:10:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :roter, :name, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_studnet(name),grade)
+    self.roster[grade] << name
+     if roster.has_key?(grade)
+       roster[grade] << name
+     else
+       roster[grade] = name
+       
+  end
+end
